1f565157f73c320123bfdde93d27b2e947e71e35
priyabratakhandual <priyabratakhandual7@gmail.com>
2025-09-25 10:38:27 +0530

    push new code

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,14 +5,8 @@ errorlog = f'{os.getcwd()}/logs/gunicorn_error.log'
 accesslog = f'{os.getcwd()}/logs/gunicorn_access.log'
 capture_output = True
 
-# Number of worker processes
 workers = 1
-
-# The type of workers to use
 worker_class = 'uvicorn.workers.UvicornWorker'
 
-# The socket to bind
-bind = 'unix:/home/ubuntu/tata-webhook/app.sock'
-
-# Set umask for socket file
-umask = 0o007
+# Bind to TCP for Docker networking
+bind = '0.0.0.0:8000'
